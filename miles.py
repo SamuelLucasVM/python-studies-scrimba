19490da4c9425cb8bca41b6acb88189a145a9d17
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2022-02-05 23:15:48 -0300

    style: mudando o texto

--- miles.py
+++ miles.py
@@ -4,4 +4,4 @@ import string
 name = input('Enter your first name:\n')
 distance = float(input('Enter the distance in km:\n'))
 
-print(f'{name.capitalize()} {distance} km or {distance/1609} miles.')
+print(f'Hi {name.capitalize()}! {distance} km is equivalent to {distance/1.609} miles.')
