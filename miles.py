c7ddac1eec5d432776262fab30ad6bade3e7c51a
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2022-02-05 22:56:36 -0300

    fix: corrigindo calculo de milha

--- miles.py
+++ miles.py
@@ -4,4 +4,4 @@ import string
 name = input('Enter your first name:\n')
 distance = float(input('Enter the distance in km:\n'))
 
-print(name.capitalize() + ', ' + str(distance) + ' ' + 'km or ' + str(distance*1609) + ' miles.')
+print(name.capitalize() + ', ' + str(distance) + ' ' + 'km or ' + str(distance/1609) + ' miles.')
