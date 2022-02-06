a142a6515eebc3c17249ca687f16919844095307
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2022-02-05 22:53:50 -0300

    feat: convertendo km para milhas

---
+++ miles.py
@@ -0,0 +1,7 @@
+import string
+
+
+name = input('Enter your first name:\n')
+distance = float(input('Enter the distance in km:\n'))
+
+print(name.capitalize() + ', ' + str(distance) + ' ' + 'km or ' + str(distance*1609) + ' miles.')
