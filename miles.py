1bfc97fb5c65b4d39225435f757ddfccfb97f98a
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2022-02-05 23:19:35 -0300

    style: arrendondando a milha para 2 casas decimais

--- miles.py
+++ miles.py
@@ -4,4 +4,4 @@ import string
 name = input('Enter your first name:\n')
 distance = float(input('Enter the distance in km:\n'))
 
-print(f'Hi {name.capitalize()}! {distance} km is equivalent to {distance/1.609} miles.')
+print(f'Hi {name.capitalize()}! {distance} km is equivalent to {round(distance/1.609,2)} miles.')
