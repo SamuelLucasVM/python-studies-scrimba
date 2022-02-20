0f9ee389aae3baf845e7066d2dca5e0f63001ac5
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2022-02-20 10:54:52 -0300

    style: diminuindo o codigo através de mudança de inputs

--- for.py
+++ for.py
@@ -8,8 +8,9 @@ names1 = ['graHam chapman', 'TERRY', 'terry jones']
 ##Adicionar uma input box para colocar mais 2 nomes na lista
 ##Printar convites para um aniversario com essas listas com 'for loops'.
 
-added = [input('What is the guest is name?\n'), input('What is the another guest is name?\n')]
-guest = (names.__add__(names1)).__add__(added)
+guest = (names.__add__(names1))
+for index in range(2):
+    guest.append(input('What is the guest name?\n'))
 
 for name in guest:
     print(f'Hello {name.title()}, you were invited to my birthday party!')
